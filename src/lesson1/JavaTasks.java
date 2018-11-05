3641d934bdac935e4bdc99833d1093bd65366434
TIMUR <etiru@yandex.ru>
2018-11-05 20:56:06 +0300

    task 2

--- src/lesson1/JavaTasks.java
+++ src/lesson1/JavaTasks.java
@@ -2,6 +2,11 @@ package lesson1;
 
 import kotlin.NotImplementedError;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 @SuppressWarnings("unused")
 public class JavaTasks {
     /**
@@ -33,7 +38,49 @@ public class JavaTasks {
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
     static public void sortTimes(String inputName, String outputName) {
-        throw new NotImplementedError();
+        Scanner inputFile;
+        try {
+            inputFile = new Scanner(new File(inputName));
+        } catch (Exception e){
+            throw new IllegalArgumentException(inputName + "Error File not found" + e);
+        }
+        ArrayList<Integer> time = new ArrayList<Integer>();
+        try{
+            FileInputStream fstream = new FileInputStream(inputName);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+
+            while ((strLine = br.readLine()) != null){
+                if(!strLine.matches("^([0-1]\\d:[0-5]\\d:[0-5]\\d)|(2[0-4]:[0-5]\\d:[0-5]\\d)$"))
+                    throw new IllegalArgumentException("error Неверный формат строки");
+                String[] split = strLine.split(":");
+                int numberOfSeconds = Integer.parseInt(split[0]) * 3600 + Integer.parseInt(split[1]) * 60 +
+                        Integer.parseInt(split[2]);
+                time.add(numberOfSeconds);
+            }
+        }catch (IOException e){
+            System.out.println("Ошибка");
+        }
+
+        int[] arrIntTime = new int[time.size()];
+        for(int i = 0; i < arrIntTime.length; i++){
+            arrIntTime[i] = time.get(i).intValue();
+        }
+
+        Sorts.quickSort(arrIntTime);
+
+        try {
+            PrintWriter writer = new PrintWriter(outputName, String.valueOf(StandardCharsets.UTF_8));
+            for (int i = 0; i < time.size(); i++){
+                int hour = time.get(i) / 3600;
+                int minutes = (time.get(i) - hour * 3600) / 60;
+                int seconds = time.get(i) - hour * 3600 - minutes * 60;
+                writer.println(String.format("%02d:%02d:%02d", hour, minutes, seconds));
+            }
+            writer.close();
+        } catch (IOException e){
+            System.out.println("Файл не создан");
+        }
     }
 
     /**
@@ -97,7 +144,77 @@ public class JavaTasks {
      * 121.3
      */
     static public void sortTemperatures(String inputName, String outputName) {
-        throw new NotImplementedError();
+        Scanner inputFile;
+        try {
+            inputFile = new Scanner(new File(inputName));
+        } catch (Exception e){
+            throw new IllegalArgumentException(inputName + "Error File not found" + e);
+        }
+
+        ArrayList<Double> temp = new ArrayList<>();
+
+        try{
+            FileInputStream fstream = new FileInputStream(inputName);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+
+            while ((strLine = br.readLine()) != null){
+                if(!strLine.matches("^(0|((-)?((\\d+)\\.\\d)))$")) {
+                    throw new IllegalArgumentException("error Неверный формат строки");
+                }
+                temp.add(Double.parseDouble(strLine));
+            }
+        }catch (IOException e){
+            System.out.println("Ошибка");
+        }
+
+        double[] tempDouble = new double[temp.size()];
+
+        for (int i = 0; i < temp.size(); i++){
+            tempDouble[i] = temp.get(i);
+        }
+        quickSort(tempDouble, 0, tempDouble.length - 1);
+        try {
+            PrintWriter writer = new PrintWriter(outputName, String.valueOf(StandardCharsets.UTF_8));
+            for (int i = 0; i < tempDouble.length; i++){
+                writer.println(tempDouble[i]);
+            }
+            writer.close();
+        } catch (IOException e){
+            System.out.println("Файл не создан");
+        }
+    }
+
+    // Метод быстрой сортировки для массива double, для метода sortTemperatures
+    private static void quickSort(double[] arr, int low, int high) {
+        if (arr == null || arr.length == 0)
+            return;
+        if (low >= high)
+            return;
+
+        int middle = low + (high - low) / 2;
+        double pivot = arr[middle];
+        int i = low, j = high;
+
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+            while (arr[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                double temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (low < j)
+            quickSort(arr, low, j);
+        if (high > i)
+            quickSort(arr, i, high);
     }
 
     /**
